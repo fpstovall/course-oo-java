6e81553d16b550bd5ba09d694f479cffce5c0ad0
Rick Stovall <fpstovall@gmail.com>
2017-02-05 17:05:29 -0500

    Task #6 : setting the starting point to local data.

--- UCSDUnfoldingMaps/src/module6/EarthquakeCityMap.java
+++ UCSDUnfoldingMaps/src/module6/EarthquakeCityMap.java
@@ -36,12 +36,10 @@ public class EarthquakeCityMap extends PApplet {
 	private static final long serialVersionUID = 1L;
 
 	// IF YOU ARE WORKING OFFILINE, change the value of this variable to true
-	private static final boolean offline = false;
+	private static final boolean offline = true;
 	
 	/** This is where to find the local tiles, for working without an Internet connection */
 	public static String mbTilesString = "blankLight-1-3.mbtiles";
-	
-	
 
 	//feed with magnitude 2.5+ Earthquakes
 	private String earthquakesURL = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.atom";
